d025f02bbc92eab7a4d1104a2a0ecc8c09d93319
Jorge Vera <jorge.vera@hey.com>
2022-10-28 22:41:53 -0400

    Set prettier semi false

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,22 +3,22 @@
  * This is only a minimal backend to get started.
  */
 
-import helmet from "helmet";
-import * as path from 'path';
-import * as express from 'express';
+import helmet from 'helmet'
+import * as path from 'path'
+import * as express from 'express'
 
-const app = express();
+const app = express()
 
 app.use(helmet())
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')))
 
 app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
-});
+  res.send({ message: 'Welcome to api!' })
+})
 
-const port = process.env.port || 3333;
+const port = process.env.port || 3333
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
-});
-server.on('error', console.error);
+  console.log(`Listening at http://localhost:${port}/api`)
+})
+server.on('error', console.error)
